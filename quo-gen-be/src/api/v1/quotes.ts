99c2e8a9babf58bf5fc265e774a9199ae0523c38
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-09-30 16:01:18 +0200

    Create 3 error messages

--- quo-gen-be/src/api/v1/quotes.ts
+++ quo-gen-be/src/api/v1/quotes.ts
@@ -9,21 +9,25 @@ const router = Router();
 
 router.get("/random", async (req, res) => {
   try {
-    const randomQuote = getRandomQuote<Quote>(QUOTES_LIST);
-    if(Object.keys(req.query).length !== 0){
-      const results = req.query.results
-      if(typeof results === "string"){
-        const inputResult = parseInt(results)
-        console.log(inputResult)
-        const quotes = getMultipleRandomQuotes<Quote>(QUOTES_LIST, inputResult);
-        res.status(200).json({
-          quotes
-        })
-      }
+    if (Object.keys(req.query).length === 0) {
+      const randomQuote = getRandomQuote<Quote>(QUOTES_LIST);
+      return res.json({
+        quote: randomQuote,
+      });
     }
-    res.json({
-      quote: randomQuote,
-    });
+    const results = req.query.results
+    if (typeof results === "undefined") return res.status(500).json({error: "Wrong query parameter"})
+
+    if (typeof results !== "string") return res.status(500).json({error:"Query parameter must be a string"})
+
+    if (isNaN(Number(results))) return res.status(500).json({error: "String must be a number"})
+
+    const inputResult = parseInt(results)
+    console.log({inputResult})
+    const quotes = getMultipleRandomQuotes<Quote>(QUOTES_LIST, inputResult);
+    res.status(200).json({
+    quotes
+        })
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch all quotes" });
   }
